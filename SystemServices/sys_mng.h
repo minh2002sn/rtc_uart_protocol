516bf333b70d367a0291b651d1e1fba5ef370a2e
khoinguyenthanh-itr-intern <nguyen.thnhkhoi@gmail.com>
2024-07-20 21:03:22 +0700

    chore(sys_mng.h): remove header file
    remove #include drv_ds1307.h

--- SystemServices/sys_mng.h
+++ SystemServices/sys_mng.h
@@ -24,7 +24,6 @@
 #include "common.h"
 #include "sys_data_mng.h"
 #include "bsp_ds1307.h"
-#include "drv_ds1307.h"
 #include "sys_data_mng_msg_define.h"
 #include "sys_data_mng_conn_define.h"
 #include "epoch_time.h"
