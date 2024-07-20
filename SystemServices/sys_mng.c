ac8930725c25abbb0d713126bf4c709c0ffb5895
khoinguyenthanh-itr-intern <nguyen.thnhkhoi@gmail.com>
2024-07-20 11:09:19 +0700

    wip(sys_mng.c): add init system manager function

--- SystemServices/sys_mng.c
+++ SystemServices/sys_mng.c
@@ -19,18 +19,33 @@
 #include "common.h"
 
 /* Private defines ---------------------------------------------------- */
+#define SYS_MNG_CBUFFER_SIZE (20) /*Size of circular buffer*/
 
 /* Private enumerate/structure ---------------------------------------- */
 
 /* Private macros ----------------------------------------------------- */
 
 /* Public variables --------------------------------------------------- */
+static cbuffer_t smng_cb;
+static uint8_t   smng_cb_buf[SYS_MNG_CBUFFER_SIZE] = { 0 };
 
 /* Private variables -------------------------------------------------- */
 
 /* Private function prototypes ---------------------------------------- */
 
 /* Function definitions ----------------------------------------------- */
+uint32_t sys_mng_init()
+{
+  uint32_t ret;
+
+  ret = cb_init(&smng_cb, smng_cb_buf, SYS_MNG_CBUFFER_SIZE);
+  ASSERT(ret == CB_SUCCESS, SYS_MNG_ERROR);
+
+  ret = sys_data_mng_reg_node(SYS_DATA_MNG_CONN_UART_TO_MNG, &smng_cb_buf);
+  ASSERT(ret == SYS_COM_SUCCES, SYS_MNG_ERROR);
+
+  return SYS_MNG_SUCCESS;
+}
 
 /* Private definitions ----------------------------------------------- */
 
