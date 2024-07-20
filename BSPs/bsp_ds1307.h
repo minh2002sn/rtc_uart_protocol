88dcd33d1ee5744e588e00a384248e87461e4630
Pham Hung <duchung15082003@gmail.com>
2024-07-21 01:53:25 +0700

    update(bsp_ds1307.h): Update bsp header file

--- BSPs/bsp_ds1307.h
+++ BSPs/bsp_ds1307.h
@@ -8,7 +8,7 @@
  * @author     Hung Pham Duc
  * @author     Khoi Thanh Nguyen
  *
- * @brief      none
+ * @brief      Board Support Package for DS1307 RTC
  *
  * @note       none
  *
@@ -21,6 +21,7 @@
 #define __BSP_DS1307_H
 
 /* Includes ----------------------------------------------------------- */
+#include "drv_ds1307.h"
 
 /* Public defines ----------------------------------------------------- */
 
@@ -31,6 +32,80 @@
 /* Public variables --------------------------------------------------- */
 
 /* Public function prototypes ----------------------------------------- */
+/**
+ * @brief         Initialize DS1307 hardware interface
+ *
+ * @param[in]     hi2c      Pointer to I2C_HandleTypeDef structure
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+uint32_t bsp_ds1307_init(I2C_HandleTypeDef *hi2c);
+
+/**
+ * @brief         Set DS1307 time through BSP layer
+ *
+ * @param[in]     time      Pointer to drv_ds1307_time_t structure
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+uint32_t bsp_ds1307_set_time(drv_ds1307_time_t *time);
+
+/**
+ * @brief         Get DS1307 time through BSP layer
+ *
+ * @param[out]    time      Pointer to drv_ds1307_time_t structure
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+uint32_t bsp_ds1307_get_time(drv_ds1307_time_t *time);
+
+/**
+ * @brief         Set DS1307 date through BSP layer
+ *
+ * @param[in]     date      Pointer to drv_ds1307_date_t structure
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+uint32_t bsp_ds1307_set_date(drv_ds1307_date_t *date);
+
+/**
+ * @brief         Get DS1307 date through BSP layer
+ *
+ * @param[out]    date      Pointer to drv_ds1307_date_t structure
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+uint32_t bsp_ds1307_get_date(drv_ds1307_date_t *date);
+
+/**
+ * @brief         Enable DS1307 oscillator frequency through BSP layer
+ *
+ * @param[in]     osci_freq   Oscillator frequency
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+uint32_t bsp_ds1307_enable_osci(drv_ds1307_osci_freq_t osci_freq);
+
+/**
+ * @brief         Disable DS1307 oscillator frequency through BSP layer
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+uint32_t bsp_ds1307_disable_osci(void);
 
 #endif // __BSP_DS1307_H
 
