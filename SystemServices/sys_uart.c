634b38cdc4aee5d667df593904b18f04e59ef81e
khoinguyenthanh-itr-intern <nguyen.thnhkhoi@gmail.com>
2024-07-21 16:02:04 +0700

    wip(sys_uart.c): define message size, data index, add circular buffer and connection init function

--- SystemServices/sys_uart.c
+++ SystemServices/sys_uart.c
@@ -22,6 +22,19 @@
 #include "common.h"
 
 /* Private defines ---------------------------------------------------- */
+#define SYS_UART_CBUFFER_SIZE (20) /*Size of circular buffer*/
+#define SYS_UART_MESSAGE_SIZE (5)  /*Size of data message*/
+
+#define SYS_UART_MESSAGE_EVENT_INDEX     (0) /*Index of event in message buffer*/
+#define SYS_UART_MESSAGE_DATE_DATA_INDEX (1) /*Index of date in message buffer*/
+#define SYS_UART_MESSAGE_MONTH_DATA_INDEX \
+  (2) /*Index of month in message buffer*/
+#define SYS_UART_MESSAGE_YEAR_DATA_INDEX (3) /*Index of year in message buffer*/
+#define SYS_UART_MESSAGE_HOUR_DATA_INDEX (4) /*Index of hour in message buffer*/
+#define SYS_UART_MESSAGE_MINUTE_DATA_INDEX \
+  (5) /*Index of minute in message buffer*/
+#define SYS_UART_MESSAGE_SECOND_DATA_INDEX \
+  (6) /*Index of second in message buffer*/
 
 /* Private enumerate/structure ---------------------------------------- */
 
@@ -31,6 +44,8 @@
 
 /* Private variables -------------------------------------------------- */
 static bsp_uart_t suart;
+static cbuffer_t  suart_cb;
+static uint8_t    suart_cb_buf[SYS_UART_CBUFFER_SIZE];
 
 /* Private function prototypes ---------------------------------------- */
 
@@ -39,9 +54,15 @@ uint32_t sys_uart_init(UART_HandleTypeDef *huart)
 {
   uint32_t ret;
 
-  // Init uart button
+  // Initialize UART
   ret = bsp_uart_init(&suart, huart);
   ASSERT(ret == BSP_UART_SUCCESS, SYS_UART_ERROR);
+  // Initialize circular buffer
+  ret = cb_init(&suart_cb, suart_cb_buf, SYS_UART_CBUFFER_SIZE);
+  ASSERT(ret == CB_SUCCESS, SYS_UART_ERROR);
+  // Initialize connection to system mangager
+  ret = sys_data_mng_reg_node(SYS_DATA_MNG_CONN_MNG_TO_UART, &suart_cb);
+  ASSERT(ret == SYS_COM_SUCCES, SYS_UART_ERROR);
 
   return SYS_UART_SUCCESS;
 }
